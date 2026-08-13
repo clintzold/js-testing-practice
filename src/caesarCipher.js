74ef8705f1b99110707fb5cc4b1e32e9c9f10856
clint zold <214383878+clintzold@users.noreply.github.com>
2026-08-12 22:43:27 -0600

    complete test suite for cipher module and full ciphering functionality

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,27 +1,58 @@
 // ./src/caesarCipher.js
 
-const alphabet = [...'abcdefghijklmnopqrstuvwlxyz']
+// Master alphabet array
+const alphabet = [...'abcdefghijklmnopqrstuvwxyz']
 
 function cipher(str, num) {
+
+  // Limit shift amount(prevents having to solve with loop or recursive function)
+  if (shiftExceeded(num)) {
+    throw new Error(`Shift value of ${num} exceeds limit of 25`)
+  }
+
+  // Store string param in array
   const strArray = [...str]
+  // Initialize array to contain cyphered characters
   const cipherArray = []
 
+  // Compare string char to master array and calculate proper cipher index positions
   strArray.forEach(char => {
+    // Retrieve calculated index based on shift param
     const index = calcIndex(char, num)
 
-    if (/[A-Z]/.test(char)) {
+    // Handle preservation of case and non-letter characters
+    if (upperCaseLetter(char)) {
       cipherArray.push(alphabet[index].toUpperCase())
+    } else if (notLetter(char)) {
+      cipherArray.push(char)
     } else {
       cipherArray.push(alphabet[index])
     }
   })
 
+  // Transform cipher back into a complete string and return the value
   return cipherArray.join('')
 }
 
 function calcIndex(char, num) {
   const cipherIndex = alphabet.findIndex(letter => letter === char.toLowerCase()) + num
-  return cipherIndex
+  if (cipherIndex > 25) {
+    return cipherIndex - 26
+  } else {
+    return cipherIndex
+  }
+}
+
+function notLetter(char) {
+  return /[^a-zA-Z]/.test(char)
+}
+
+function upperCaseLetter(char) {
+  return /[A-Z]/.test(char)
+}
+
+function shiftExceeded(num) {
+  return num > 25
 }
 
 export { cipher }
--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -11,11 +11,30 @@ test('Takes a letter and returns another letter based on shift value', () => {
   expect(cipher('b', 6)).toBe('h')
 })
 
-test('Takes a multiple letters and returns a string of shifted letters by shift factor param', () => {
+test('Takes multiple letters and returns a string of shifted letters by shift factor param', () => {
   expect(cipher('abc', 3)).toBe('def')
 })
 
 test('Takes a string with mixed casing and returns correct character cipher', () => {
   expect(cipher('aBc', 3)).toBe('dEf')
   expect(cipher('heLLo', 3)).toBe('khOOr')
+})
+
+test('cipher ignores and returns empty space, symbols and numbers', () => {
+  expect(cipher('hello world.', 3)).toBe('khoor zruog.')
+  expect(cipher('hello, world.', 3)).toBe('khoor, zruog.')
+  expect(cipher('hello, world!', 3)).toBe('khoor, zruog!')
+})
+
+test('cipher wraps back to beginning of alphabet', () => {
+  expect(cipher('z', 3)).toBe('c')
+})
+
+test('throws error if shift number exceeds 25', () => {
+  expect(() => cipher('a', 26)).toThrow()
+})
+
+test('handles shift values up to and including 26 and returns correct letter', () => {
+  expect(cipher('a', 25)).toBe('z')
+  expect(cipher('a, bc', 25)).toBe('z, ab')
 })
